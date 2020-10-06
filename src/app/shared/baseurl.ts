2d7825b950d97a9de81a8bccd1c5c943e47f15b3
Alex Williamson <alexwilliamson123@gmail.com>
2020-10-05 22:34:39 -0700

    changed npm version

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,2 +1,2 @@
-export const baseURL = 'http://paypulsereall-env.eba-dfgdnuw7.us-east-2.elasticbeanstalk.com/';
-// export const baseURL = 'http://localhost:3000/';
+// export const baseURL = 'http://paypulsereall-env.eba-dfgdnuw7.us-east-2.elasticbeanstalk.com/';
+export const baseURL = 'http://localhost:3000/';
