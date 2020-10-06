fb091d24b8928ce2a195324cc2ca959f2f18ea08
Alex Williamson <alexwilliamson123@gmail.com>
2020-10-06 12:43:14 -0700

    one more againnnn

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@
 /**
  * Module dependencies.
  */
-var app = require('./backend/app');
+var app = require('./app');
 var debug = require('debug')('pay-pulse');
 var http = require('http');
 // var https = require('https');
