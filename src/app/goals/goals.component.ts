3be47ba8d8364e1ebee0dd44c6b7407960d19fb7
Alex Williamson <alexwilliamson123@gmail.com>
2020-11-09 18:39:29 -0800

    columns on goals now resize

--- src/app/goals/goals.component.ts
+++ src/app/goals/goals.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Goal } from '../shared/goal';
 
 @Component({
@@ -10,6 +10,10 @@ export class GoalsComponent implements OnInit {
   recentlyCompletedGoals: Goal[];
   monthlyGoals: Goal[];
   allGoals: Goal[];
+
+  columns: number = 4;
+
+  @ViewChild('box') box: ElementRef;
 
   constructor() { }
 
@@ -50,6 +54,27 @@ export class GoalsComponent implements OnInit {
       {_id: "9", userId: "1", goalName: "i"},
       {_id: "10", userId: "1", goalName: "j"},
     ];
+  }
+
+  onBoxResize() {
+    if(this.box) {
+      console.log(this.box.nativeElement.clientWidth)
+      var currWidth = this.box.nativeElement.clientWidth;
+      // let n = Math.floor(this.box.nativeElement.clientWidth / 100);
+      // this.columns = (n > 0 ? n : 1);
+      if (currWidth < 447) {
+        this.columns = 1;
+      }
+      else if (currWidth < 960) {
+        this.columns = 2;
+      }
+      else {
+        this.columns = 4;
+      }
+    } 
+    // else {
+    //   this.columns = 1;
+    // }
   }
 
 }
